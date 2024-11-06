feacca3025ff227d7770c5641f241481bee2fa4f
sky4564 <qw486512@gmail.com>
2024-11-06 17:15:04 +0900

    feat : Add interceptors to axios

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
   baseURL: "http://ec2-43-201-110-116.ap-northeast-2.compute.amazonaws.com:8080/",
@@ -6,22 +7,78 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
   withCredentials: true,
-})
+});
+
+// 요청 인터셉터
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    // 토큰이 만료되었을 때 (401 에러)
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = Cookies.get('refreshToken');
+        const response = await axios.post('/users/refresh', {
+          refreshToken: refreshToken
+        });
+
+        if (response.data.accessToken) {
+          Cookies.set('accessToken', response.data.accessToken, {
+            expires: 7,
+            secure: true,
+            sameSite: 'strict'
+          });
+
+          originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+          return axiosInstance(originalRequest);
+        }
+      } catch (refreshError) {
+        Cookies.remove('accessToken');
+        Cookies.remove('refreshToken');
+        window.location.href = '/login_bm';
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 const fetchData = async (url: string, options = {}) => {
   try {
-    const response = await axiosInstance(url, options)
-    return response.data
+    const response = await axiosInstance({
+      url,
+      ...options,
+    });
+    return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       // AxiosError인 경우에만 접근
-      return error.response?.data
+      return error.response?.data;
     } else {
       // AxiosError가 아닌 경우
-      return { message: "Unknown error occurred" }
+      return { message: "Unknown error occurred" };
     }
   }
+};
 
-}
-
-export { fetchData }
+export { fetchData };
