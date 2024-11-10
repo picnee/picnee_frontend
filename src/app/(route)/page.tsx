350947de739c46098a54ddec964440a0e7533eb4
yeooonn <rladuswjd0303@naver.com>
2024-11-10 22:04:02 +0900

    Feat: 베스트 리뷰 맛집 컴포넌트 UI 추가

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,4 +1,5 @@
 import Main from "../pages/main";
+
 export default function Home() {
   return <Main />;
 }
--- src/app/pages/main/index.tsx
+++ src/app/pages/main/index.tsx
@@ -1,3 +1,4 @@
+import BestReviewRestaurant from "./_components/BestReviewRestaurant";
 import LocationBox from "./_components/LocationBox";
 import MainCategoryBox from "./_components/MainCategoryBox";
 import MainInputBox from "./_components/MainInputBox";
@@ -9,6 +10,9 @@ const Main = () => {
         <MainInputBox />
         <MainCategoryBox />
         <LocationBox />
+      </section>
+      <section className="w-[1280px] h-auto px-[40px] flex flex-col justify-center  gap-10 mt-16 ">
+        <BestReviewRestaurant />
       </section>
     </div>
   );
