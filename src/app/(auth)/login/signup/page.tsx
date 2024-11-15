67638c44a5c137561685b7bc82acdfbacb3aed85
sunghyun1014 <sunghyun1014@naver.com>
2024-11-16 00:10:14 +0900

    feat: 회원가입 페이지 추가

--- src/app/(auth)/login/signup/page.tsx
+++ src/app/(auth)/login/signup/page.tsx
@@ -1,5 +1,15 @@
-export default function SignUpPage() {
-    return (
-        <div>sign up page</div>
-    )
-}
+import React from "react";
+import RegistForm from "./RegistForm";
+
+const page = () => {
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className="w-[384px]">
+        <h1 className="text-center text-[32px] font-bold">이메일로 회원가입</h1>
+        <RegistForm />
+      </div>
+    </div>
+  );
+};
+
+export default page;
