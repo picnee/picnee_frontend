bd917357b4a28ea23b0420342a539928be8e6fc1
yeooonn <rladuswjd0303@naver.com>
2024-12-01 00:54:00 +0900

    Feat: 좋아요 공통 컴포넌트 추가

---
+++ src/components/common/Like.tsx
@@ -0,0 +1,14 @@
+interface PropsType {
+  likeNum: number | string;
+}
+
+const Like = ({ likeNum }: PropsType) => {
+  return (
+    <div className="flex gap-[10px]">
+      <span className="block w-[24px] h-[24px] bg-gray-200"></span>
+      <span>{likeNum}</span>
+    </div>
+  );
+};
+
+export default Like;
