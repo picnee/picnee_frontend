0a59ea321100aae1c1b9b305dee2a932463d0f72
yeooonn <rladuswjd0303@naver.com>
2024-12-05 02:13:54 +0900

    feat: 여행토크 필터링 기능 추가
    - 초기 로드 시 전체 게시글 데이터를 호출
    - 카테고리 및 지역 선택 시 데이터 필터링 기능 구현
    - 필터 변경에 따라 게시글 리스트가 동적으로 갱신되도록 처리

--- src/app/(route)/travelTalk/_components/MyCommentTalkList.tsx
+++ src/app/(route)/travelTalk/_components/MyCommentTalkList.tsx
@@ -1,46 +1,50 @@
 import Sticker from "@/components/common/Sticker";
+import useFormatTimeAgo from "@/hooks/useFormatTimeAgo";
 
 interface dataType {
-  comment: string;
-  sticker: string;
+  boardRes: { boardCategory: string; boardId: string; region: string };
+  commentsCount: number;
+  content: string;
+  createdAt: string;
+  postId: string;
   title: string;
-  date: string;
+  userRes: { userId: string; nickName: string };
+  viewed: number;
 }
 
 interface MyCommentTalkListProps {
   data: dataType[];
-  selectedWriteMenu: string;
 }
 
-const MyCommentTalkList = ({
-  data,
-  selectedWriteMenu,
-}: MyCommentTalkListProps) => {
+const MyCommentTalkList = ({ data }: MyCommentTalkListProps) => {
   return (
     <>
-      {data.map((item: dataType) => (
-        <div className="mb-[16px]" key={item.comment}>
-          <div className="w-[100%] h-[auto] p-[22px] border border-gray-150 rounded-m box-border">
-            <div className="">
-              <div className="mb-[10px] flex justify-between">
-                <p
-                  title={item.comment}
-                  className="font-400 text-2xl text-black truncate max-w-[633px] whitespace-nowrap"
-                >
-                  {item.comment}
-                </p>
-                <p className="font-500 text-sm text-gray-400">{item.date}</p>
-              </div>
-              <div className="flex gap-[8px]">
-                <Sticker title={item.sticker} />
-                <p className="font-400 text-lg text-gray-400 truncate max-w-[633px] whitespace-nowrap">
-                  {item.title}
-                </p>
+      {data &&
+        data.map((item: dataType) => (
+          <div className="mb-[16px]" key={item.boardRes.boardId}>
+            <div className="w-[100%] h-[auto] p-[22px] border border-gray-150 rounded-m box-border">
+              <div className="">
+                <div className="mb-[10px] flex justify-between">
+                  <p
+                    title={item.content}
+                    className="font-400 text-2xl text-black truncate max-w-[633px] whitespace-nowrap"
+                  >
+                    {item.content}
+                  </p>
+                  <p className="font-500 text-sm text-gray-400">
+                    {useFormatTimeAgo(item.createdAt)}
+                  </p>
+                </div>
+                <div className="flex gap-[8px]">
+                  <Sticker title={item.boardRes.boardCategory} />
+                  <p className="font-400 text-lg text-gray-400 truncate max-w-[633px] whitespace-nowrap">
+                    {item.title}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };
