338790591b0eea20ac3898c1e0342442553ef11b
yeooonn <rladuswjd0303@naver.com>
2024-12-09 01:37:52 +0900

    Remove: 사용 안하는 쿼리 옵션 제거

--- src/api/travelTalk/query-options.tsx
+++ src/api/travelTalk/query-options.tsx
@@ -1,33 +1,20 @@
-import { CreateTravelTalkPost } from '@/app/(route)/travelTalk/actions/CreateTravelTalkPost';
-import { GetTravelTalkDetailData } from '@/app/(route)/travelTalk/detail/[postId]/actions/GetTravelTalkDetailData';
-import { GetTravelTalkListData } from '@/app/(route)/travelTalk/actions/GetTravelTalkListData';
+"use client";
+import { GetTravelTalkDetailData } from "@/app/(route)/travelTalk/detail/[postId]/actions/GetTravelTalkDetailData";
+import { GetTravelTalkListData } from "@/app/(route)/travelTalk/actions/GetTravelTalkListData";
 import {
-  CreateTravelTalkParamsType,
   TravelTalkCommentParamsType,
   TravelTalkDetailPostParamsType,
   TravelTalkRequestParamsType,
-} from '@/types/travelTalk';
-import { GetTravelTalkCommentData } from '@/app/(route)/travelTalk/detail/[postId]/actions/GetTravelTalkCommentData';
+} from "@/types/travelTalk";
+import { GetTravelTalkCommentData } from "@/app/(route)/travelTalk/detail/[postId]/actions/GetTravelTalkCommentData";
 
 /** 여행 토크 게시글 리스트 GET */
 export function GetTravelTalkListOptions(
   requestParams: TravelTalkRequestParamsType
 ) {
   return {
-    queryKey: ['travelTalkList', requestParams],
+    queryKey: ["travelTalkList", requestParams],
     queryFn: () => GetTravelTalkListData(requestParams),
-    staleTime: 1000 * 60 * 10, // 10분
-    catchTime: 1000 * 60 * 60 * 24, // 24시간
-  };
-}
-
-/** 여행 토크 게시글 등록 POST */
-export function CreateTravelTalkPostOptions(
-  requestParams: CreateTravelTalkParamsType
-) {
-  return {
-    queryKey: ['travelTalkDetailData', requestParams],
-    queryFn: () => CreateTravelTalkPost(requestParams),
     staleTime: 1000 * 60 * 10, // 10분
     catchTime: 1000 * 60 * 60 * 24, // 24시간
   };
@@ -38,7 +25,7 @@ export function GetTravelTalkDetailPostOptions(
   requestParams: TravelTalkDetailPostParamsType
 ) {
   return {
-    queryKey: ['travelTalkDetailData', requestParams],
+    queryKey: ["travelTalkDetailData", requestParams],
     queryFn: () => GetTravelTalkDetailData(requestParams),
     staleTime: 1000 * 60 * 10, // 10분
     catchTime: 1000 * 60 * 60 * 24, // 24시간
@@ -50,7 +37,7 @@ export function GetTravelTalkCommentOptions(
   requestParams: TravelTalkCommentParamsType
 ) {
   return {
-    queryKey: ['travelTalkComment', requestParams],
+    queryKey: ["travelTalkComment", requestParams],
     queryFn: () => GetTravelTalkCommentData(requestParams),
     staleTime: 1000 * 60 * 10, // 10분
     catchTime: 1000 * 60 * 60 * 24, // 24시간
