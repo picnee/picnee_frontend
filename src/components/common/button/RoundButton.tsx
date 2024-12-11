e6ef214f2969b8700c38ea199b5310427b8bff4b
yeooonn <rladuswjd0303@naver.com>
2024-12-11 18:23:12 +0900

    Design: 라운드 버튼 CSS 수정

--- src/components/common/button/RoundButton.tsx
+++ src/components/common/button/RoundButton.tsx
@@ -7,15 +7,13 @@ interface PropsType {
 
 const RoundButton = ({ text, hasIcon }: PropsType) => {
   return (
-    <button className="h-[40px] text-gray-500 border border-gray-150 rounded-[50px] pt-[4px] pl-[16px] pb-[4px] pr-[16px]">
+    <button className="h-[40px] flex items-center text-gray-500 border border-gray-150 rounded-[50px] pt-[4px] pl-[16px] pb-[4px] pr-[16px]">
       {hasIcon && (
         <span
-          className={`w-[24px] h-[24px] mr-[4px] inline-block ${
-            hasIcon && "mt-[3px]"
-          } bg-gray-150`}
+          className={`w-[24px] h-[24px] mr-[4px] inline-block bg-gray-150`}
         ></span>
       )}
-      <span className={`${hasIcon && "relative top-[-6px]"}`}>{text}</span>
+      <p className={`whitespace-nowrap`}>{text}</p>
     </button>
   );
 };
