82b0fa7bc307538a6c05c808f72218fa092f34eb
yeooonn <rladuswjd0303@naver.com>
2024-12-11 20:33:56 +0900

    Feat: 라운드 버튼 클릭 액션 추가

--- src/components/common/button/RoundButton.tsx
+++ src/components/common/button/RoundButton.tsx
@@ -3,9 +3,15 @@ import { memo } from "react";
 interface PropsType {
   text: string;
   hasIcon: boolean;
+  onClick?: () => void;
 }
 
-const RoundButton = ({ text, hasIcon }: PropsType) => {
+const RoundButton = ({ text, hasIcon, onClick }: PropsType) => {
+  const onClickButton = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
   return (
     <button className="h-[40px] flex items-center text-gray-500 border border-gray-150 rounded-[50px] pt-[4px] pl-[16px] pb-[4px] pr-[16px]">
       {hasIcon && (
@@ -13,7 +19,9 @@ const RoundButton = ({ text, hasIcon }: PropsType) => {
           className={`w-[24px] h-[24px] mr-[4px] inline-block bg-gray-150`}
         ></span>
       )}
-      <p className={`whitespace-nowrap`}>{text}</p>
+      <p className={`whitespace-nowrap`} onClick={onClickButton}>
+        {text}
+      </p>
     </button>
   );
 };
