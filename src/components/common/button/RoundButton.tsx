d0d60b84a0eabf69110620c2b7fb765af2dc0318
yeooonn <rladuswjd0303@naver.com>
2024-12-22 19:59:09 +0900

    Feat: 아이콘 이름 받아오는 props 추가

--- src/components/common/button/RoundButton.tsx
+++ src/components/common/button/RoundButton.tsx
@@ -1,12 +1,19 @@
+import Icon from "@/public/svgs/Icon";
 import { memo } from "react";
 
 interface PropsType {
-  text: string;
+  text?: string;
   hasIcon: boolean;
+  iconName?: string;
   onClick?: () => void;
 }
 
-const RoundButton = ({ text, hasIcon, onClick }: PropsType) => {
+const RoundButton = ({
+  text = "",
+  hasIcon = false,
+  iconName = "",
+  onClick,
+}: PropsType) => {
   const onClickButton = () => {
     if (onClick) {
       onClick();
@@ -16,12 +23,19 @@ const RoundButton = ({ text, hasIcon, onClick }: PropsType) => {
     <button className="h-[40px] flex items-center text-gray-500 border border-gray-150 rounded-[50px] pt-[4px] pl-[16px] pb-[4px] pr-[16px]">
       {hasIcon && (
         <span
-          className={`w-[24px] h-[24px] mr-[4px] inline-block bg-gray-150`}
-        ></span>
+          className={`${
+            iconName === "" &&
+            "w-[24px] h-[24px] mr-[4px] inline-block bg-gray-150"
+          }`}
+        >
+          {iconName && <Icon iconName={iconName} />}
+        </span>
       )}
-      <p className={`whitespace-nowrap`} onClick={onClickButton}>
-        {text}
-      </p>
+      {text && (
+        <p className={`whitespace-nowrap`} onClick={onClickButton}>
+          {text}
+        </p>
+      )}
     </button>
   );
 };
