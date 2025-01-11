cebc0bf2fad0c51299a940ca195a99322aebf5e5
yeooonn <rladuswjd0303@naver.com>
2025-01-11 16:56:29 +0900

    Chore: react-query-devtools 설치

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useRef } from "react";
 
 export default function QueryProvider({
@@ -22,6 +23,7 @@ export default function QueryProvider({
 
   return (
     <QueryClientProvider client={queryClientRef.current}>
+      <ReactQueryDevtools initialIsOpen={true} />
       {children}
     </QueryClientProvider>
   );
